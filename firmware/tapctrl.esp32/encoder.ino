6387767c4bbd37f11f53b814729c3787e3f8fab0
John Spencer <john.spencer.9000@gmail.com>
2021-10-27 22:35:22 +1100

    cleanup and adding photos

--- firmware/tapctrl.esp32/encoder.ino
+++
@@ -1,9 +0,0 @@
-/*
- * Deal with the rotary encoder here.
- */
-
-#include <ESP32Encoder.h>
-
-// Rotary encoder
-ESP32Encoder encoder;
-long oldPosition  = -999;
--- firmware/tapctrl.esp32/i2c.ino
+++
@@ -1,9 +0,0 @@
-/*
- * Config for the i2c Master Sender/Reciver
- */
-
-#include <Wire.h>
-
-void setup_i2c() {
-  Wire.begin(); // join i2c bus (address optional for master)
-}
